7377d3b98678085ba202f450d03b604eca7a7f31
Ajay Sehwal <110731336+ajaysehwal@users.noreply.github.com>
2023-03-07 16:12:39 +0000

    dp

--- Minimum number of coins.js
+++ Minimum number of coins.js
@@ -7,7 +7,7 @@ var coinChange = function(coins, amount) {
     const dp=Array(amount+1).fill(Infinity);
 
     dp[0]=0;
-    console.log(dp)
+    console.log(dp);
     for(let el of coins){
         for(let j=el;j<=amount;j++){
             dp[j]=Math.min(dp[j],dp[j-el]+1);
