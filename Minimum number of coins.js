9b05344f263115d80f88b4f9ac067c9994a59a09
Ajay Sehwal <110731336+ajaysehwal@users.noreply.github.com>
2023-03-07 16:12:15 +0000

    Initial commit

---
+++ Minimum number of coins.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function(coins, amount) {
+    const dp=Array(amount+1).fill(Infinity);
+
+    dp[0]=0;
+    console.log(dp)
+    for(let el of coins){
+        for(let j=el;j<=amount;j++){
+            dp[j]=Math.min(dp[j],dp[j-el]+1);
+            console
+        }
+    }
+     
+     return dp[amount]==Infinity?-1:dp[amount]
+};
+
